1455d0eb123a639b3d1050995cca1214c9eaeae8
naoki.tomita <6903308+naoki-tomita@users.noreply.github.com>
2025-10-31 20:16:40 +0900

    ズーム

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
     <html lang="en">
       <head>
         <title>Tter</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0" />
       </head>
       <body>
         <style>{`a { text-decoration: none; }`}</style>

052af49daddd388bd5e7f62bfeb5e5bc7af330f9
naoki.tomita <6903308+naoki-tomita@users.noreply.github.com>
2025-10-31 22:04:12 +0900

    Fontええかんじ

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
       <head>
         <title>Tter</title>
         <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=RocknRoll+One&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet" />
       </head>
       <body>
         <style>{`a { text-decoration: none; }`}</style>
